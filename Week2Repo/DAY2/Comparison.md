d6bdf3801a2c9d8abf1359aba14f577593c71fa7
Shivang Jha <shivang.jha@hestabit.in>
2025-11-19 19:13:38 +0530

    Removed squashed Week2Repo import

--- Week2Repo/DAY2/Comparison.md
+++
@@ -1,5 +0,0 @@
-1) The given reference image
-    ![alt text](<Screenshot from 2025-11-11 12-34-49.png>)
-2) The screenshot of final UI
-    ![alt text](image.png)
-    ![alt text](image-1.png)
--- Week2Repo/DAY3/app.js
+++
@@ -1,9 +0,0 @@
-const questions = document.querySelectorAll('.question');
-
-questions.forEach(q =>{
-    const btn = q.querySelector('.qButton');
-    const ans = q.querySelector('.ans');
-    btn.addEventListener('click', ()=>{
-        ans.classList.toggle('show');
-    });
-});
