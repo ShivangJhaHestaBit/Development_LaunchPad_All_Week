d45b263036a3d53fdf643835a6ba5138ee95d532
Shivang Jha <shivang.jha@hestabit.in>
2025-11-19 15:49:56 +0530

    Day 5 Deliverables

---
+++ WEEK1/DAY5/index.js
@@ -0,0 +1,10 @@
+const userName = 'Shivang';
+function greetUser(name) {
+  return `Hello, ${name}!`;
+}
+
+const greeting = greetUser(userName);
+function goodbyeUser(name) {
+  return `Bye, ${name}!`;
+}
+console.log(greeting);
